c3c15d234b0c8ba1e0a20e8c8c1d982da4368510
queen <Queenmoncler@gmail.com>
2023-12-05 10:53:27 +0300

    initial commit

---
+++ src/main/java/com/example/netology_diplom_moneytransferservice/NetologyDiplomMoneytransferserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.netology_diplom_moneytransferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetologyDiplomMoneytransferserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetologyDiplomMoneytransferserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/netology_diplom_moneytransferservice/NetologyDiplomMoneytransferserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netology_diplom_moneytransferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetologyDiplomMoneytransferserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
